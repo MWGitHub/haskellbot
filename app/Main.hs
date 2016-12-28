e3cca03d82d78fa016aee8a0b810f88066bfa18f
Michael Wang <mwwpublic@gmail.com>
2016-12-28 18:13:41 -0500

    add rtm connection

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Web.Slack
+import Web.Slack.Message
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+import Control.Applicative
+
+config :: String -> SlackConfig
+config apiToken = SlackConfig
+       { _slackApiToken = apiToken -- Specify your API token here
+       }
+
+haskellBot :: SlackBot ()
+haskellBot (Message cid _ msg _ _ _) = sendMessage cid msg
+haskellBot _ = return ()
 
 main :: IO ()
-main = someFunc
+main = do
+  apiToken <- fromMaybe (error "SLACK_API_TOKEN not set")
+               <$> lookupEnv "SLACK_API_TOKEN"
+  runBot (config apiToken) haskellBot ()
